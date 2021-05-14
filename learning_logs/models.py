8195a58fd08c6213ffc206dc22342d29418a717d
Robert-Alexandru Kiss <ionoobmaster@hotmail.com>
2021-05-14 21:38:25 +0300

    main

---
+++ learning_logs/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+class Topic(models.Model):
+    """A topic for the user is learning about"""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.text
+
+class Entry(models.Model):
+    """Something specific about this topic."""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entires'
+
+    def __str__(self):
+        """Return a string respresentation of the model."""
+        if len(self.text) > 50:
+            return f"{self.text[:50]}..."
+        else:
+            return f"{self.text}"
