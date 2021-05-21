b088b57d9c11a5bcb4bb6c8cf414f789d909caca
Robert-Alexandru Kiss <ionoobmaster@hotmail.com>
2021-05-21 14:24:54 +0300

    new stuff

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -10,6 +10,12 @@ urlpatterns = [
     path('', views.index, name='index'),
     # Page shows all topics
     path('topics/', views.topics, name='topics'),
-    #Detail page for a single topic
+    # Detail page for a single topic
     path ('topics/<int:topic_id>/', views.topic, name='topic'),
+    # Page for adding new topic
+    path('new_topic/', views.new_topic, name='new_topic'),
+    # Page for adding new entry
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
+    # Path for editing entry
+    path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),
 ]
